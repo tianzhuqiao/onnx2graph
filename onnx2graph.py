dae4fd7106a01ee32aa754fc8fa3ab1776967f42
Tianzhu Qiao <tq@feiyilin.com>
2021-07-20 14:23:20 -0700

    Update graphviz.

--- onnx2graph.py
+++ onnx2graph.py
@@ -23,28 +23,49 @@ class Graph:
         self.update_nodes()
         self.update_inout_blocks(self.graph.node)
 
+    def get_all_nodes(self):
+        initializers = [n.name for n in self.graph.initializer]
+        # get the "required" inputs
+        inputs = [n.name for n in self.graph.input if n.name not in initializers]
+
+        nodes = [n.name for n in self.graph.node if n.op_type != 'Constant']
+        output = [n.name for n in self.graph.output]
+        return inputs + nodes + output
+
+    def get_name(self, name):
+        if name.endswith('bias'):
+            return 'B'
+        elif name.endswith('weight'):
+            return 'W'
+        elif name.endswith('beta'):
+            return 'beta'
+        elif name.endswith('gamma'):
+            return 'gamma'
+        elif name.endswith('mean'):
+            return 'mean'
+        elif name.endswith('var'):
+            return 'var'
+        return name
+
+    def shape_str(self, shape):
+        return 'x'.join([str(d) for d in shape])
+
     def print_graph(self):
-        G = pgv.AGraph(directed=True)
-        to_nodes = ['Input73']
-        done_nodes = {}
+
         template_table = '''<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">{rows}</TABLE>>'''
-        template_header = '''<TR><TD BGCOLOR="grey">{name}</TD></TR>'''
+        template_header = '''<TR><TD BGCOLOR="grey" ALIGN="left">{name}</TD></TR>'''
         template_row = '''<TR><TD ALIGN="left">{name}</TD></TR>'''
-        while to_nodes:
-            name = to_nodes[0]
-            done_nodes[to_nodes[0]] = True
-            node = self.nodes[to_nodes.pop(0)]
+
+        G = pgv.AGraph(directed=True)
+
+        all_nodes = self.get_all_nodes()
+        for nd in self.get_all_nodes():
+            name = nd
+            node = self.nodes[name]
             print(f'{node.op}({name})')
 
             if 'output_blocks' not in node:
                 node.output_blocks = []
-            for out in node.output_blocks:
-                if out not in done_nodes:
-                    to_nodes.append(out)
-            if name in self.connections:
-                for out in self.connections[name].outputs:
-                    if out not in done_nodes:
-                        to_nodes.append(out)
             if 'input' not in node:
                 node.input = []
             inputs = []
@@ -52,7 +73,8 @@ class Graph:
                 input = [i]
                 if i in self.connections and self.connections[i].inputs:
                     input = self.connections[i].inputs
-                inputs.append(template_row.format(name=f'In: {input[0]} ({self.nodes[i].shape})'))
+                if input[0] not in all_nodes:
+                    inputs.append(template_row.format(name=f'<b>{self.get_name(input[0])}</b> &lt;{self.shape_str(self.nodes[i].shape)}&gt;'))
                 print(f'   in: {input} {self.nodes[i].shape}')
             if not node.output:
                 node.output = []
@@ -61,43 +83,46 @@ class Graph:
                 output = [o]
                 if o in self.connections and self.connections[o].outputs:
                     output = self.connections[o].outputs
-                outputs.append(template_row.format(name=f'Out:{output[0]} ({self.nodes[o].shape})'))
+                if output[0] not in all_nodes:
+                    outputs.append(template_row.format(name=f'Out:{self.get_name(output[0])} ({self.shape_str(self.nodes[o].shape)})'))
                 print(f'   out:{output} {self.nodes[o].shape}')
-            header = template_header.format(name=f'{node.op} ({name})')
+            header = template_header.format(name=f'{node.op}')
             rows = "\n".join([header] + inputs + outputs)
             G.add_node(f'{name}', label=template_table.format(rows=rows), shape='plaintext')
 
-        all_nodes = done_nodes
         # add edges
-        to_nodes = ['Input73']
-        done_nodes = {}
-        while to_nodes:
-            name = to_nodes[0]
-            done_nodes[to_nodes[0]] = True
-            node = self.nodes[to_nodes.pop(0)]
+        for nd in self.get_all_nodes():
+            name = nd
+            node = self.nodes[name]
 
-            for out in node.output_blocks:
-                if out not in done_nodes:
-                    to_nodes.append(out)
-            if name in self.connections:
-                for out in self.connections[name].outputs:
-                    if out not in done_nodes:
-                        to_nodes.append(out)
+            if 'output_blocks' not in node:
+                node.output_blocks = []
 
+            if 'input' not in node:
+                node.input = []
             for i in node.input:
                 input = [i]
                 if i in self.connections and self.connections[i].inputs:
                     input = self.connections[i].inputs
                 if input[0] in all_nodes:
-                    G.add_edge(input[0], name)
 
+                    if self.nodes[input[0]].shape:
+                        G.add_edge(input[0], name, label=self.shape_str(self.nodes[input[0]].shape))
+                    else:
+                        G.add_edge(input[0], name)
+
+            if not node.output:
+                node.output = []
             outputs = []
             for o in node.output:
                 output = [o]
                 if o in self.connections and self.connections[o].outputs:
                     output = self.connections[o].outputs
                 if output[0] in all_nodes:
-                    G.add_edge(name, output[0])
+                    if node.shape:
+                        G.add_edge(name, output[0], label=self.shape_str(node.shape))
+                    else:
+                        G.add_edge(name, output[0])
 
         G.layout(prog='dot')
         G.write("mnist.dot")
@@ -199,5 +224,5 @@ class Graph:
         return values
 
 if __name__ == "__main__":
-    model = ONNX2Graph('mnist.onnx')
+    model = ONNX2Graph('resnet18-v1-7.onnx')
     model.graph.print_graph()
