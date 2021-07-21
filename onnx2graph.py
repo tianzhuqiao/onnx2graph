5d1750e2a6071b8e2b59772c68460ca5813513a7
Tianzhu Qiao <tq@feiyilin.com>
2021-07-20 21:18:00 -0700

    Update layer shape.

--- onnx2graph.py
+++ onnx2graph.py
@@ -23,6 +23,9 @@ class Graph:
         self.update_nodes()
         self.update_inout_blocks(self.graph.node)
 
+        self.update_attributes()
+        self.cal_shapes()
+
     def get_all_nodes(self):
         initializers = [n.name for n in self.graph.initializer]
         # get the "required" inputs
@@ -50,6 +53,113 @@ class Graph:
     def shape_str(self, shape):
         return 'x'.join([str(d) for d in shape])
 
+    def cal_shapes(self):
+        for node in self.graph.node:
+            if self.nodes[node.name].shape is None:
+                shape = self.calc_shape(node)
+                if shape is not None:
+                    self.nodes[node.name].shape = shape
+
+    def calc_shape(self, node):
+        attributes = self.nodes[node.name].attributes
+        if node.op_type == "Conv":
+            weight = None
+            input = None
+            for i in self.nodes[node.name].input:
+                if i.endswith('weight') or self.nodes[i].op == 'Constant':
+                    weight = self.nodes[i].shape
+                else:
+                    input = self.nodes[i].shape
+            attributes = self.nodes[node.name].attributes
+            stride = attributes.strides
+            padding = attributes.pads or [0, 0, 0, 0]
+            kernal = attributes.kernel_shape
+            shape = [0]*(len(kernal))
+            for i in range(len(kernal)):
+                shape[i] = int((input[2+i] - kernal[i] + padding[2*i] + padding[2*i+1])/stride[i]+1)
+            return [input[0], weight[0]] + shape
+        elif node.op_type in ['BatchNormalization', 'Relu', 'Div', 'Add']:
+            input = None
+            for i in self.nodes[node.name].input:
+                input = self.nodes[i].shape
+                break
+            if input:
+                return input
+        elif node.op_type in ['MaxPool']:
+            input = None
+            for i in self.nodes[node.name].input:
+                input = self.nodes[i].shape
+                break
+            attributes = self.nodes[node.name].attributes
+            kernel = attributes.kernel_shape
+            pads = attributes.pads
+            strides = attributes.strides
+            shape = [0]*len(kernel)
+            for i in range(len(kernel)):
+                shape[i] = int((input[2+i] - kernel[i] + pads[2*i] + pads[2*i+1])/strides[i] + 1)
+            return input[0:2] + shape
+        elif node.op_type in ['Add']:
+            shape = None
+            for i in self.nodes[node.name].input:
+                if shape is None:
+                    shape = self.nodes[i].shape
+                    continue
+                assert shape == self.nodes[i].shape
+            return shape
+        elif node.op_type in ['GlobalAveragePool']:
+            input = None
+            for i in self.nodes[node.name].input:
+                input = self.nodes[i].shape
+                break
+            shape = list(input)
+            shape[-2:] = [1, 1]
+            return shape
+        elif node.op_type in ['Flatten']:
+            input = None
+            for i in self.nodes[node.name].input:
+                input = self.nodes[i].shape
+                break
+            return input[:-2]
+        elif node.op_type in ['Reshape']:
+            shape = attributes.shape
+            return shape
+        elif node.op_type in ['MatMul']:
+            shape = None
+            for i in self.nodes[node.name].input:
+                s = self.get_shape(i)
+                if shape is None:
+                    shape = self.get_shape(i)
+                    continue
+                assert shape[-1] == s[0]
+                shape = shape[:-1] + s[1:]
+            return shape
+        return None
+
+    def update_attributes(self):
+        for node in self.graph.node:
+            self.get_attributes(node)
+
+    def get_attributes(self, node):
+        for attrib in node.attribute:
+            if "attributes" not in self.nodes[node.name]:
+                self.nodes[node.name]['attributes'] = _dict()
+            self.nodes[node.name]['attributes'][attrib.name] = self.get_attribute(attrib)
+
+    def get_attribute(self, attribute):
+        if attribute.type == onnx.AttributeProto.AttributeType.INTS:
+            return attribute.ints
+        elif attribute.type == onnx.AttributeProto.AttributeType.INT:
+            return attribute.i
+        elif attribute.type == onnx.AttributeProto.AttributeType.FLOAT:
+            return attribute.f
+        elif attribute.type == onnx.AttributeProto.AttributeType.TENSOR:
+            return numpy_helper.to_array(attribute.t)
+        elif attribute.type == onnx.AttributeProto.AttributeType.STRING:
+            return attribute.s
+
+        assert False
+        return None
+
     def print_graph(self):
 
         template_table = '''<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">{rows}</TABLE>>'''
@@ -74,7 +184,11 @@ class Graph:
                 if i in self.connections and self.connections[i].inputs:
                     input = self.connections[i].inputs
                 if input[0] not in all_nodes:
-                    inputs.append(template_row.format(name=f'<b>{self.get_name(input[0])}</b> &lt;{self.shape_str(self.nodes[i].shape)}&gt;'))
+                    in_name = self.get_name(input[0])
+                    if self.nodes[i].op == "Constant":
+                        if in_name == input[0]:
+                            in_name = "C"
+                    inputs.append(template_row.format(name=f'<b>{in_name}</b> &lt;{self.shape_str(self.nodes[i].shape)}&gt;'))
                 print(f'   in: {input} {self.nodes[i].shape}')
             if not node.output:
                 node.output = []
@@ -195,6 +309,12 @@ class Graph:
                 self.nodes[node.name] = _dict(name=node.name)
             self.nodes[node.name].update(kwargs)
             self.nodes[node.name].shape = self.get_node_shape(node)
+    def get_shape(self, name):
+        shape = self.nodes[name].shape
+        if not shape and name in self.connections:
+            input = self.connections[name]['inputs'][0]
+            shape = self.nodes[input].shape
+        return shape
 
     def get_node_shape(self, node):
         # get type of input tensor
@@ -225,4 +345,5 @@ class Graph:
 
 if __name__ == "__main__":
     model = ONNX2Graph('resnet18-v1-7.onnx')
+    #model = ONNX2Graph('mnist.onnx')
     model.graph.print_graph()
